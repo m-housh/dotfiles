5cbb1affd50b187923c17111188462d9e40aea7d
Michael Housh <m-housh@users.noreply.github.com>
2023-09-18 22:35:40 -0400

    Allow hidden files to be shown in neo-git configuration

--- nvim/lua/plugin-config/neo-tree.lua
+++ nvim/lua/plugin-config/neo-tree.lua
@@ -1,16 +1,19 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      require("neo-tree").setup({
-        close_if_last_window = false,
-        enable_git_status = true,
-        enable_diagnostics = true
-      })
-    end
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    close_if_last_window = false,
+    enable_git_status = true,
+    enable_diagnostics = true,
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false
+      }
+    }
+  }
 }
