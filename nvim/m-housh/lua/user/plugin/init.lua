56cfaf436047de8f03d124cb4ae4725b49e8eccb
Michael Housh <mhoush@houshhomeenergy.com>
2023-09-28 10:31:25 -0400

    Fixed .gitignore

---
+++ nvim/m-housh/lua/user/plugin/init.lua
@@ -0,0 +1,30 @@
+-- This file contains plugin's that don't require much configuration.
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    version = "2.*",
+    build = "make install_jsregexp",
+    config = function()
+      local luasnip = require('luasnip')
+      local types = require('luasnip.util.types')
+      luasnip.config.set_config {
+        history = true,
+        updateevents = "TextChanged,TextChangedI",
+        enable_autosnippets = true,
+        ext_opts = {
+          [types.choiceNode] = {
+            active = {
+              virt_text = { { "<-", "Error" } },
+            },
+          },
+        },
+      }
+    end
+  },
+  {
+	  "hrsh7th/vim-vsnip",
+	  config = function()
+      vim.g.vsnip_snippet_dir = os.getenv('HOME') .. '/.config/m-housh/lua/snippets/'
+    end
+  },
+}
---
+++ nvim/m-housh/lua/user/plugin/wrapping.lua
@@ -0,0 +1,20 @@
+return {
+    "andrewferrier/wrapping.nvim",
+    opts = {
+      auto_set_mode_filetype_allowlist = {
+        "asciidoc",
+        "gitcommit",
+        "latex",
+        "markdown",
+        "tex",
+        "text"
+      },
+    },
+    config = function()
+      require("wrapping").setup({
+        softener = {
+          markdown = 1.3
+        },
+      })
+    end
+  }
