385c17811d631f05d73b9a677b3a674f0e74ce11
Michael Housh <m-housh@users.noreply.github.com>
2023-10-05 20:51:11 -0400

    Update timeout settings for nvim

--- nvim/m-housh/lua/user/settings.lua
+++ nvim/m-housh/lua/user/settings.lua
@@ -3,9 +3,9 @@ local wo = vim.wo
 local fn = vim.fn
 
 vim.cmd.set('inccommand=split')
-o.updatetime = 300 -- faster completion
---o.timeoutlen = 500 -- time to wait for a mapped sequence to complete (in milliseconds)
---o.ttimeoutlen = 50 -- Time in milliseconds to wait for a key code sequence to complete
+o.updatetime = 500 -- faster completion
+o.timeoutlen = 500 -- time to wait for a mapped sequence to complete (in milliseconds)
+o.ttimeoutlen = 300 -- Time in milliseconds to wait for a key code sequence to complete
 o.backup = false -- creates a backup file
 o.swapfile = false -- enable/disable swap file creation
 o.dir = fn.stdpath("data") .. "/swp" -- swap file directory
