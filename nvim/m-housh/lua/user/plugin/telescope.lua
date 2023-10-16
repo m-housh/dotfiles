892966d27a975ade90582e5555d049197e880571
Michael Housh <mhoush@houshhomeenergy.com>
2023-10-16 13:03:36 -0400

    Adds .swiftpm to be ignored in telescope

--- nvim/m-housh/lua/user/plugin/telescope.lua
+++ nvim/m-housh/lua/user/plugin/telescope.lua
@@ -10,7 +10,7 @@ return {
 
     require('telescope').setup({
       defaults = {
-        file_ignore_patterns = {"node_modules", "%.jpg", "%.png"},
+        file_ignore_patterns = {"node_modules", "%.jpg", "%.png", ".swiftpm"},
         vimgrep_arguments = {
           'rg',
           '--follow',
