016e4d459fe2e0460b56906da9143d70d40267b3
Michael Housh <mhoush@houshhomeenergy.com>
2023-10-22 14:07:05 -0400

    fixes: typo in swift plugin

--- nvim/m-housh/lua/user/plugin/swift.lua
+++ nvim/m-housh/lua/user/plugin/swift.lua
@@ -3,7 +3,7 @@ return {
     dependencies = {
       'akinsho/toggleterm.nvim'
     },
-    ft = { 'swfit' },
+    ft = { 'swift' },
     config = function()
       require('swift').setup()
       local default_options = { noremap = true, silent = true }
