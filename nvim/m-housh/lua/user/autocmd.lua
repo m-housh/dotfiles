8e46610a7bb1a6d66b62d35b38a4683980ddb60b
Michael Housh <mhoush@houshhomeenergy.com>
2023-11-07 08:15:20 -0500

    updated nvim autocmd

--- nvim/m-housh/lua/user/autocmd.lua
+++ nvim/m-housh/lua/user/autocmd.lua
@@ -60,22 +60,22 @@ createCmd(
 )
 
 -- Swift
-createCmd(
-  "FileType",
-  {
-    pattern = { 'swift' },
-    callback = function()
-      local root_dir = vim.fs.dirname(vim.fs.find({
-        "Package.swift",
-        ".git",
-      }, { upward = true })[1])
-      local client = vim.lsp.start({
-        name = "sourcekit-lsp",
-        cmd = { "sourcekit-lsp" },
-        root_dir = root_dir
-      })
-      vim.lsp.buf_attach_client(0, client)
-    end,
-    group = swiftGroup
-  }
-)
+-- createCmd(
+--   "FileType",
+--   {
+--     pattern = { 'swift' },
+--     callback = function()
+--       local root_dir = vim.fs.dirname(vim.fs.find({
+--         "Package.swift",
+--         ".git",
+--       }, { upward = true })[1])
+--       local client = vim.lsp.start({
+--         name = "sourcekit-lsp",
+--         cmd = { "sourcekit-lsp" },
+--         root_dir = root_dir
+--       })
+--       vim.lsp.buf_attach_client(0, client)
+--     end,
+--     group = swiftGroup
+--   }
+-- )
