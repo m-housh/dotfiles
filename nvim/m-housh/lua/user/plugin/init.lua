fb235ab19d1ea23b483de9928f865dacc6d67cd9
Michael Housh <mhoush@houshhomeenergy.com>
2024-03-20 18:03:05 -0400

    removes lua-snip

--- nvim/m-housh/lua/user/plugin/init.lua
+++ nvim/m-housh/lua/user/plugin/init.lua
@@ -1,27 +1,27 @@
 -- This file contains plugin's that don't require much configuration.
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    version = "2.*",
-    build = "make install_jsregexp",
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local luasnip = require('luasnip')
-      local types = require('luasnip.util.types')
-      luasnip.config.set_config {
-        history = true,
-        updateevents = "TextChanged,TextChangedI",
-        enable_autosnippets = true,
-        ext_opts = {
-          [types.choiceNode] = {
-            active = {
-              virt_text = { { "<-", "Error" } },
-            },
-          },
-        },
-      }
-    end
-  },
+--   {
+--     "L3MON4D3/LuaSnip",
+--     version = "2.*",
+--     build = "make install_jsregexp",
+--     event = { 'BufReadPre', 'BufNewFile' },
+--     config = function()
+--       local luasnip = require('luasnip')
+--       local types = require('luasnip.util.types')
+--       luasnip.config.set_config {
+--         history = true,
+--         updateevents = "TextChanged,TextChangedI",
+--         enable_autosnippets = true,
+--         ext_opts = {
+--           [types.choiceNode] = {
+--             active = {
+--               virt_text = { { "<-", "Error" } },
+--             },
+--           },
+--         },
+--       }
+--     end
+--   },
   {
 	  "hrsh7th/vim-vsnip",
 	  config = function()
