8ac60b41967f8f735753c60c9789a3d3147f5937
Michael Housh <mhoush@houshhomeenergy.com>
2024-11-22 12:43:29 -0500

    feat: Adds note about keeping nvim settings at top of init.lua

--- nvim/m-housh/init.lua
+++ nvim/m-housh/init.lua
@@ -2,6 +2,10 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- NOTE: Keep settings at the top, otherwise some options don't work well.
+--       mainly have had trouble with not using the settings in an `.editorconfig`
+--       file for projects.
+
 -- Common settings
 require("user.settings")
 
