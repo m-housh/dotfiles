d27f2ad4909b170c90c69f99c2846dff8cb12144
Michael Housh <mhoush@houshhomeenergy.com>
2024-12-11 18:07:18 -0500

    feat: LSP updates

--- nvim/m-housh/lua/user/plugin/lsp.lua
+++ nvim/m-housh/lua/user/plugin/lsp.lua
@@ -19,6 +19,15 @@ return {
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
+		{
+			"folke/lazydev.nvim",
+			ft = "lua",
+			opts = {
+				library = {
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
 	},
 	config = function()
 		require("mason").setup()
