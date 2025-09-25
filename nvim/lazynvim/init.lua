2ea4540cba4edc5dc2fdcfc8eaef3c2ac9016d04
Michael Housh <michael@mhoush.com>
2025-09-25 11:05:01 -0400

    feat: Updates install package script to include what should be needed on a fresh install, changes pinentry program which was causing ssh not to work.

--- nvim/lazynvim/init.lua
+++ nvim/lazynvim/init.lua
@@ -1,2 +1,3 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+-- bootstrap lazy.nvim, LazyVim and your plugin
+vim.g.netrw_browsex_viewer = "xdg-open"
 require("config.lazy")
--- scripts/install-pkg-arch.sh
+++ scripts/install-pkg-arch.sh
@@ -7,5 +7,67 @@
 #       need to be revisited to ensure packages installed before this
 #       script started are included.
 
+# Basics
+yay -S --noconfirm --needed \
+  asahi-desktop-meta \
+  base-devel \
+  bat \
+  btop \
+  catppuccin-gtk-theme-mocha \
+  eza \
+  fastfetch \
+  fzf \
+  git \
+  github-cli \
+  gum \
+  hyprpaper \
+  hyrpidle \
+  hyprlock \
+  neovim \
+  nwg-look \
+  pcsc-controls \
+  starship \
+  swaync \
+  tmux \
+  zoxide \
+  zsh
+
+# Terminals
+yay -S --noconfirm ghostty-git kitty
+
 # Clipboard history utility
 yay -S --noconfirm clipse wl-clipboard
+
+# Menu bar
+yay -S --noconfirm waybar
+
+# Fonts
+yay -S --noconfirm \
+  ttf-jetbrains-mono-nerd \
+  ttf-firacode \
+  ttf-inconsolata-nerd
+
+# Application launcher
+yay -S --noconfirm walker elephant
+
+# Bluetooth management
+yay -S --noconfirm blueberry-wayland
+
+# terminal based password manager.
+yay -S --noconfirm gopass-git git-credential-gopass
+
+# Audio stuff
+yay -S --noconfirm pipewire \
+  pavucontrol \
+  wireplumber \
+  pipewire-jack \
+  pipewire-pulse
+
+# Browsers
+yay -S --noconfirm zen-browser-bin chromium
+
+# File managers / explorers
+yay -S --noconfirm yazi nautilus
+
+# Email
+yay -S --noconfirm thunderbird
