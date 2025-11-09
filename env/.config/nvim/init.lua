18fc260a754fb64577ad0530a875acf5889baa67
Michael Housh <michael@mhoush.com>
2025-11-08 22:49:05 -0500

    feat: Adds docker language server to nvim config.

--- env/.config/nvim/init.lua
+++ env/.config/nvim/init.lua
@@ -51,12 +51,35 @@ vim.cmd [[set completeopt+=menuone,noselect,popup]]
 
 -- LSP
 vim.lsp.enable({
-	"lua_ls", "tinymist", "marksman", "bashls", "hyprls",
+	"lua_ls", "tinymist", "marksman", "bashls", "hyprls", "docker-language-server",
 })
 
 vim.lsp.config('bashls', {
 	filetypes = { "bash", "sh", "zsh" },
 	cmd = { 'bash-language-server', 'start' },
+})
+
+vim.lsp.config('docker-language-server', {
+	cmd = { 'docker-language-server', 'start', '--stdio' },
+	filetypes = { 'dockerfile', 'yaml.docker-compose' },
+	get_language_id = function(_, ftype)
+		if ftype == 'yaml.docker-compose' or ftype:lower():find('ya?ml') then
+			return 'dockercompose'
+		else
+			return ftype
+		end
+	end,
+	root_markers = {
+		'Dockerfile',
+		'docker-compose.yaml',
+		'docker-compose.yml',
+		'compose.yaml',
+		'compose.yml',
+		'docker-bake.json',
+		'docker-bake.hcl',
+		'docker-bake.override.json',
+		'docker-bake.override.hcl',
+	},
 })
 
 -- Fix warnings for 'vim' global keyword.
